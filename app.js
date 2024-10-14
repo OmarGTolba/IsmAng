5dc6195aaacf46cdb52cfab60e3eada787b81687
OmarGTolba <omar.739.tolba@gmail.com>
2024-10-14 19:27:56 +0300

    my commit

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ console.log('hello from omar');
 
 
 console.log('first commit');
+console.log('omar  commit');
 
+
+// git checkout -b "omar"
+
+// git add . 
+
+// git commit -m "commit Name"
+
+//  git push 
